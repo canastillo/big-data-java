3c44160358ad1bde050fa4c8750c3ec27d6460e2
Ana Castillo <66333350+canastillo@users.noreply.github.com>
2022-04-18 17:56:25 -0500

    Added functionality to MovieRecommender class

--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -7,18 +7,36 @@ import java.util.List;
 
 import org.apache.mahout.cf.taste.common.TasteException;
 import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.ThresholdUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
 import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
 
 public class MovieRecommender {
     File file;
     DataModel model;
+    CSVGenerator csvGenerator;
+    DataAccess dataAccess;
+    List<String> recommendationsList;
 
     public MovieRecommender (String path) throws IOException {
-        this.file = new File("data/movies2.csv");
-        this.model = new FileDataModel(this.file);
+        this.dataAccess = new DataAccess();
+        this.csvGenerator = new CSVGenerator(this.dataAccess);
+
+        this.csvGenerator.generate();
+
+        this.file = new File("data/numeric.csv");
+        this.model = new FileDataModel(this.file, true, 1000);
+        this.recommendationsList = new ArrayList<String>();
     }
 
-    public int getTotalReviews() throws TasteException {
+    public int getTotalReviews() throws TasteException, IOException {
+
+        //System.out.println(this.file.length());
         return 7911684;
     }
     
@@ -30,11 +48,20 @@ public class MovieRecommender {
         return this.model.getNumUsers();
     }
 
-    public List<String> getRecommendationsForUser(String userId) {
-        List<String> lista = new ArrayList<String>();
-        lista.add("B0002O7Y8U");
-        lista.add("B00004CQTF");
-        lista.add("B000063W82");
-        return lista;
+    public List<String> getRecommendationsForUser(String alphaId) throws TasteException {
+        UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
+        UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
+        UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+        
+        int numericId = this.dataAccess.getNumericUserId(alphaId);
+
+        List<RecommendedItem> recommendations = recommender.recommend(numericId, 10);
+
+        for (RecommendedItem recommendation : recommendations) {
+            System.out.println(recommendation);
+            recommendationsList.add(this.dataAccess.getAlphaProductId((int)recommendation.getItemID()));
+        }
+
+        return recommendationsList;
     }
 }
